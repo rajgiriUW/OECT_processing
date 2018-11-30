2a1f21ab935ba3ee6be7f54b65ff2f19f52053f2
Raj Giridharagopal <rgiri@uw.edu>
2018-11-30 13:22:30 -0800

    Equivalence bug

--- deriv.py
+++ deriv.py
@@ -8,18 +8,19 @@ Derivative for generating gm from Id-Vg plots
 
 
 def gm_deriv(v, i, method='raw', fit_params={'window': 11, 'polyorder': 2, 'deg': 8}):
-    if method is 'sg':
+
+    if method == 'sg':
         # Savitsky-Golay method
         if not fit_params['window'] & 1:  # is odd
             fit_params['window'] += 1
         gml = sps.savgol_filter(i.T, window_length=fit_params['window'],
                                 polyorder=fit_params['polyorder'], deriv=1,
                                 delta=v[2] - v[1])[0, :]
-    elif method is 'raw':
+    elif method == 'raw':
         # raw derivative
         gml = np.gradient(i.flatten(), v[2] - v[1])
 
-    elif method is 'poly':
+    elif method == 'poly':
         # polynomial fit
         funclo = np.polyfit(v, i, fit_params['deg'])
         gml = np.gradient(np.polyval(funclo, v), (v[2] - v[1]))
