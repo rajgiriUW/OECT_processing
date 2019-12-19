deba0a335c1fbb9fc76b5291ec4d08221fe0329b
Raj Giridharagopal <rgiri@uw.edu>
2019-12-18 20:20:19 -0800

    Updates to h5py reading

--- uvvis.py
+++ uvvis.py
@@ -609,17 +609,27 @@ def save_h5(data, filename):
     '''
     Saves the to two HDF5 files (.h5)
     '''
-    with h5py.File(filename+'.h5', 'w') as f:
+    
+    if isinstance(filename, str):
+        filename = Path(filename)
+    
+    with h5py.File(filename, 'w') as f:
         dset = f.create_dataset('potentials', (len(data.potentials),))
         dset[:] = data.potentials[:]
-        dset = f.create_dataset('charge', (len(data.charge.values[0]),))
-        dset[:] = data.charge.values[0][:]
+        try:
+            dset = f.create_dataset('charge', (len(data.charge.values[0]),))
+            dset[:] = data.charge.values[0][:]
+        except:
+            pass
     f.close()
     
     for p in data.spectra_vs_time:
-        data.spectra_vs_time[p].to_hdf(filename+'.h5', key=str(p), mode='a')
+        data.spectra_vs_time[p].to_hdf(filename, key=str(p), mode='a')
     
-    data.current.to_hdf(filename+'\dopingdata.h5', key ='current', mode='a')
+    try:
+        data.current.to_hdf(filename, key ='current', mode='a')
+    except:
+        pass
     
     return
 
@@ -637,18 +647,22 @@ def convert_h5(h5file):
     
     folders = []
     for f in file:
-        if 'x' in f:
-            folders.append(f[1:])
-        else:
-            folders.append(f)
-    folders = folders[1:]
-    folders_num = [float(p) for p in folders[1:]]
+        folders.append(f)
+    non_potentials = ['current', 'charge', 'potentials']
+    _fold_temp = [c if c not in non_potentials else None for c in folders]
+    folders = [c for c in _fold_temp if(c)]            
+                
+    folders_num = [float(p) for p in folders[:]]
     
     # The spectra_vs_time data
     df_dict = {}
     for v, n in zip(folders, folders_num):
-        p = 'x'+v
-        spec_file = file[p]
+        try:
+            spec_file = file[v]
+        except:
+            p = 'x'+v
+            spec_file = file[p]
+        
         df = pd.DataFrame(data = spec_file['block0_values'][()], 
                           index = spec_file['axis1'][()], 
                           columns = spec_file['axis0'])
@@ -660,18 +674,16 @@ def convert_h5(h5file):
     data.spectra_vs_time = df_dict
     
     # Now get the current data
-    current_dict = {}
-    for v, n in zip(folders, folders_num):
-        p = 'x'+v
-        spec_file = file[p]
-        df = pd.DataFrame(data = spec_file['block0_values'][()], 
-                          index = spec_file['axis1'][()], 
-                          columns = spec_file['axis0'])
-        df.index.name = 'Wavelength (nm)'
-        df.columns.name = 'Time (s)'
-        df_dict[n] = df
-        data.tx = np.round(spec_file['axis0'], 2)
+    current = pd.DataFrame(data = file['current']['block0_values'][()],
+                           index = file['current']['axis1'][()],
+                           columns = file['current']['axis0'])
+    current.index.name = 'Time (s)'
+    current.columns.name = 'Potential (V)'
         
+    data.current = current
+    
+    data.charge = pd.DataFrame(data = file['charge'][()],
+                               index = data.potentials.T)
     
     file.close()
     
