a81807f2319991e6b4616cf2c9e19fdc6e6b3ec0
Raj <rgiri@uw.edu>
2021-03-19 17:44:21 -0700

    more reorg, some import bugs

--- oect/__init__.py
+++ oect/__init__.py
@@ -1,8 +1,9 @@
 from . import oect_utils
 from . import nonoect_utils
 from . import specechem
+from .oect import OECT
+from .oect_device import OECTDevice
 
-__all__ = ['oect', 'oect_device']
-__all__ += oect_utils.__all__
+__all__ = oect_utils.__all__
 __all__ += nonoect_utils.__all__
 __all__ += specechem.__all__
---
+++ oect/oect_utils/__init__.py
@@ -0,0 +1,5 @@
+from . import deriv
+from . import oect_load
+from . import oect_plot
+
+__all__ = ['oect_load', 'oect_plot']
